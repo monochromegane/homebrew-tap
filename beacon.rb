252070a52710065cca5b627a52dbe87e47e284af
monochromegane <dev.kuro.obi@gmail.com>
2026-02-06 10:02:21 +0900

    Update beacon

--- beacon.rb
+++ beacon.rb
@@ -1,26 +1,27 @@
 class Beacon < Formula
+  desc 'beacon is a CLI tool that tracks Claude Code session states within tmux.'
   version '0.2.0'
   homepage 'https://github.com/monochromegane/beacon'
 
   on_macos do
     if Hardware::CPU.arm?
       url 'https://github.com/monochromegane/beacon/releases/download/v0.2.0/beacon_v0.2.0_darwin_arm64.zip'
-      sha256 'b38c31035b7a2cc83a24535de8f037ac0fd9fa85d8fa2f20ea98f83ea735013d'
+      sha256 '7ae7ff018620a1120b2bcc42ff164e405ee57844fac60ac0bead7c07d0040492'
     end
     if Hardware::CPU.intel?
       url 'https://github.com/monochromegane/beacon/releases/download/v0.2.0/beacon_v0.2.0_darwin_amd64.zip'
-      sha256 '3d148d39fbf2372f69396805533ecb3124c6f39108f4466478040ed591b1fd7a'
+      sha256 '885439e996ed62bf333e804ef9bf01231ae15f5e226dd2c44176accbe1cec9d2'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url 'https://github.com/monochromegane/beacon/releases/download/v0.2.0/beacon_v0.2.0_linux_arm64.tar.gz'
-      sha256 '2109e61be38c118103ae8226800d030302bd24c92179b1b145671eb969c43ea7'
+      sha256 'a44e0188c9cb35b1e5ea1d4a7d19443152655155fbaa8f8e3888d39dc856cad3'
     end
     if Hardware::CPU.intel?
       url 'https://github.com/monochromegane/beacon/releases/download/v0.2.0/beacon_v0.2.0_linux_amd64.tar.gz'
-      sha256 '350b94c3d44f1918a21e18cd43062af358dd9ed04ca3d0aea9f1e026eb57a54c'
+      sha256 '49886d6233ffc27c09b44c9a2f1c4f1a3ba0d740dcc3b43ca39aa0642a10b1cb'
     end
   end
 
